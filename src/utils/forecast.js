a9110692a3c8122b6decde8e43e34757aa965fc4
AnotherStupidDev <fx014@yandex.ru>
2020-01-28 15:43:59 +0500

    Added some about information. Added additional data into forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 
 // // encodeURIComponent - translate some symbols into URL acceptable string (in case of crashes)
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/e46aea09786f2a4366756e230e64df36/${encodeURIComponent(latitude)}`+','+`${encodeURIComponent(longitude)}?units=si&lang=ru`
+    const url = `https://api.darksky.net/forecast/e46aea09786f2a4366756e230e64df36/${encodeURIComponent(latitude)}`+','+`${encodeURIComponent(longitude)}?units=si&lang=en`
     request({url, json: true}, (error, {body}) => {
         debugger
         if (error) {
@@ -13,8 +13,9 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location. Maybe wrong coordinates?', undefined)
         } else {
             const temperature = body.currently.temperature
-            const precip = body.currently.precipProbability
-            const infa = body.daily.data[0].summary + ` It is currently ${temperature} degrees out. There is a ${precip} chance of rain`
+            const windSpeed = body.currently.windSpeed
+            const apparentTemp = body.currently.apparentTemperature
+            const infa = body.daily.data[0].summary + ` It is currently ${temperature} degrees out. Apparent temperature is ${apparentTemp} degrees out. Wind speed is ${windSpeed} m/s `
             callback(undefined,infa)
         }
     })
